6cb7fa806eff4b1e135a238ac3c3e1c15e9e5339
Peter <30659829+g126@users.noreply.github.com>
2025-04-09 11:24:49 -0400

    Create encrypt_contract_att.sh

---
+++ HPVS-AttestationSignature-files/encrypt_contract_att.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CONTRACT_KEY="/data/hpcr/config/certs/ibm-hyper-protect-container-runtime-24.11.0-encrypt.crt"
+WORKLOAD="./workload.yml"
+PASSWORD="$(openssl rand 32 | base64 -w0)"
+ENCRYPTED_PASSWORD="$(echo -n "$PASSWORD" | base64 -d | openssl rsautl -encrypt -inkey $CONTRACT_KEY -certin | base64 -w0 )"
+ENCRYPTED_WORKLOAD="$(echo -n "$PASSWORD" | base64 -d | openssl enc -aes-256-cbc -pbkdf2 -pass stdin -in "$WORKLOAD" | base64 -w0)"
+echo "workload: \"hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_WORKLOAD}\"" > user-data
+# For attestation of the workload section 
+echo "`echo "hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_WORKLOAD}" | tr -d "\n\r" | sha256sum` contract:workload" > se-checksums.txt
+
+
+#ENV="./env.yml"
+ENV="./env-syslog.yml"
+#ENV="./env-logdna-tor.yml"
+PASSWORD="$(openssl rand 32 | base64 -w0)"
+ENCRYPTED_PASSWORD="$(echo -n "$PASSWORD" | base64 -d | openssl rsautl -encrypt -inkey $CONTRACT_KEY  -certin | base64 -w0)"
+ENCRYPTED_ENV="$(echo -n "$PASSWORD" | base64 -d | openssl enc -aes-256-cbc -pbkdf2 -pass stdin -in "$ENV" | base64 -w0)"
+echo "env: \"hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_ENV}\"" >> user-data
+# For attestation of the env section
+echo "`echo "hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_ENV}" | tr -d "\n\r" | sha256sum` contract:env" >> se-checksums.txt
+
+
+ATTESTATION="./public_attestation.pem"
+PASSWORD="$(openssl rand 32 | base64 -w0)"
+ENCRYPTED_PASSWORD="$(echo -n "$PASSWORD" | base64 -d | openssl rsautl -encrypt -inkey $CONTRACT_KEY  -certin | base64 -w0)"
+ENCRYPTED_ATTESTATION="$(echo -n "$PASSWORD" | base64 -d | openssl enc -aes-256-cbc -pbkdf2 -pass stdin -in "$ATTESTATION" | base64 -w0)"
+echo "attestationPublicKey: \"hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_ATTESTATION}\"" >> user-data
+# For attestation of the Attestation section
+echo "`echo "hyper-protect-basic.${ENCRYPTED_PASSWORD}.${ENCRYPTED_ATTESTATION}" | tr -d "\n\r" | sha256sum` contract:attestationPublicKey" >> se-checksums.txt
